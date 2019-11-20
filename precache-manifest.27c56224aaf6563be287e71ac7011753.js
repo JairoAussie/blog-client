e9cf03ec2eb4c24fbd3254a526d25c645c583039
Jairo Bilbao <jairoaussie@gmail.com>
2019-11-20 11:53:41 +1000

    Updates

---
+++ precache-manifest.27c56224aaf6563be287e71ac7011753.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0faf349ba6465caad1e46635cd7f00f3",
+    "url": "/blog-client/index.html"
+  },
+  {
+    "revision": "7e23c8f691d4fb0e2c62",
+    "url": "/blog-client/static/css/2.46f0bf41.chunk.css"
+  },
+  {
+    "revision": "7e23c8f691d4fb0e2c62",
+    "url": "/blog-client/static/js/2.0a48bea9.chunk.js"
+  },
+  {
+    "revision": "e45cf8edc8d2d62c363e",
+    "url": "/blog-client/static/js/main.65c56261.chunk.js"
+  },
+  {
+    "revision": "0a2fb8f7e4203d6b14ed",
+    "url": "/blog-client/static/js/runtime-main.2689d353.js"
+  }
+]);
